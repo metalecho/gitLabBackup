629e4cc489b0e836492e3c35926dca7e3df74363
yanlu <yanlu@aspirecn.com>
2019-10-17 16:29:23 +0800

    gitlab backup shell
    Signed-off-by: yanlu <yanlu@aspirecn.com>

---
+++ gitlab_backup.sh
@@ -0,0 +1,37 @@
+#！ /bin/bash
+
+LocalBackDir=/apps/sda1/gitlab/data/backups
+
+# 备份时间戳
+Date=`date +"%F-%T"`
+
+# 备份日志目录
+LogDir=$LocalBackDir/log
+[ -d $LogDir ] || mkdir -p $LogDir
+
+# 备份日志文件
+LogFile=$LocalBackDir/log/backup_gitlab_data_$Date.log
+touch $LogFile
+
+# 打印每次备份的档案名
+echo "The files  backup to local server is:backup_gitlab_data_$Date" >> $LogFile
+
+# $?符号显示上一条命令的返回值，如果为0则代表执行成功，其他表示失败
+if [ $? -eq 0 ];then
+#追加日志到日志文件
+echo "--------------------------------Success!-------------------------------" >> $LogFile
+echo "Gitlab auto backup at local server, end at $(date +"%Y-%m-%d %H:%M:%S")" >> $LogFile
+
+else
+
+#追加日志到日志文件
+echo "--------------------------------Failed!----------------------------------" >> $LogFile
+echo "Gitlab auto backup at local server failed at $(date +"%Y-%m-%d %H:%M:%S")" >> $LogFile
+
+fi
+
+case "$1" in 
+    start)
+            docker exec gitlab gitlab-rake gitlab:backup:create
+            ;;
+esac
